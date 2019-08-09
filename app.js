a9ec2d335a219b10a4c81f5f05edb6340dec85df
ajLapid718 <ajLapid718@gmail.com>
2019-08-09 11:00:39 -0400

    Listen for requests

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // This is where our middleware and express app will live;
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.send("Hello world!");
+})
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000!!!");
+})
