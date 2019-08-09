dcf6196990e1d3c7e5cb3401a0c71d37d49543b7
ajLapid718 <ajLapid718@gmail.com>
2019-08-09 11:07:15 -0400

    Mount api router

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 // This is where our middleware and express app will live;
 const express = require("express");
 const app = express();
+const apiRouter = require("./routes");
 
-app.get("/", (req, res, next) => {
-  res.send("Hello world!");
-})
+app.use("/api", apiRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!!!");
