700041350b85c78df6044703120d98835af9f15e
ajLapid718 <ajLapid718@gmail.com>
2019-08-09 11:11:53 -0400

    GET routes work

--- routes/campuses.js
+++ routes/campuses.js
@@ -1,4 +1,8 @@
 // This is where the api endpoints for campuses will be at;
 const router = require("express").Router();
 
+router.get("/", (req, res, next) => {
+  res.send("Campuses!!!");
+})
+
 module.exports = router;
--- routes/students.js
+++ routes/students.js
@@ -1,4 +1,8 @@
 // This is where the api endpoints for students will be at;
 const router = require("express").Router();
 
+router.get("/", (req, res, next) => {
+  res.send("Students!!!");
+})
+
 module.exports = router;
